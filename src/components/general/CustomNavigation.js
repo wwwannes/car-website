456206bc740a12cfb3725e014a8e21118ff6cd30
Modix <modix@MacBook-Pro-van-Modix.local>
2022-01-25 12:46:18 +0100

    Updated hommepage layout

--- src/components/general/CustomNavigation.js
+++ src/components/general/CustomNavigation.js
@@ -1,4 +1,4 @@
-import { Grid, Link, Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
@@ -6,57 +6,60 @@ export default function CustomNavigation(){
 
     return(
         <>
-            <nav>
-                <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                        <Typography variant="h4" gutterBottom>Jimmie's Car</Typography>
+            <Container maxWidth="xl" disableGutters={true} sx={[
+                {my: 1}, 
+                {position: "fixed"}, 
+                {zIndex: 999999}, 
+                {ml: "auto"}, 
+                {mr: "auto"},
+                {left: '50%'},
+                {transform: 'translateX(-50%)'}
+            ]}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <Typography variant="h4" gutterBottom>Demo Dealer</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <nav>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={4}>
+                                        <Typography align="right" gutterBottom>
+                                            <NavLink 
+                                                to="vehicles" 
+                                                className={(navData) => navData.isActive ? "active" : "" }
+                                                style={(navData) => ({
+                                                    color: navData.isActive ? "green" : "red"
+                                                })}
+                                            >Stock</NavLink>
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Typography align="right" gutterBottom>
+                                            <NavLink 
+                                                to="philosophy" 
+                                                className={(navData) => navData.isActive ? "active" : "" }
+                                                style={(navData) => ({
+                                                    color: navData.isActive ? "green" : "red"
+                                                })}
+                                            >Philosophy</NavLink>
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={4}>
+                                        <Typography align="right" gutterBottom>
+                                            <NavLink 
+                                                to="contact" 
+                                                className={(navData) => navData.isActive ? "active" : "" }
+                                                style={(navData) => ({
+                                                    color: navData.isActive ? "green" : "red"
+                                                })}
+                                            >Contact us</NavLink>
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </nav>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={1}>
-                        <Typography align="right" gutterBottom>
-                            <NavLink 
-                                to="/" 
-                                className={(navData) => navData.isActive ? "active" : "" }
-                                style={(navData) => ({
-                                    color: navData.isActive ? "green" : "red"
-                                })}
-                            >Home</NavLink>
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <Typography align="right" gutterBottom>
-                            <NavLink 
-                                to="vehicles" 
-                                className={(navData) => navData.isActive ? "active" : "" }
-                                style={(navData) => ({
-                                    color: navData.isActive ? "green" : "red"
-                                })}
-                            >Stock</NavLink>
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <Typography align="right" gutterBottom>
-                            <NavLink 
-                                to="philosophy" 
-                                className={(navData) => navData.isActive ? "active" : "" }
-                                style={(navData) => ({
-                                    color: navData.isActive ? "green" : "red"
-                                })}
-                            >Philosophy</NavLink>
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={1}>
-                        <Typography align="right" gutterBottom>
-                            <NavLink 
-                                to="contact" 
-                                className={(navData) => navData.isActive ? "active" : "" }
-                                style={(navData) => ({
-                                    color: navData.isActive ? "green" : "red"
-                                })}
-                            >Contact us</NavLink>
-                        </Typography>
-                    </Grid>
-                </Grid>
-            </nav>
+            </Container>
         </>
     )
 }
--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,58 +1,170 @@
-import { Card, CardMedia, Container, Grid, Typography } from '@mui/material';
+import { Box, Card, CardMedia, Container, Grid, Typography } from '@mui/material';
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+import FormSelect from '../components/form/FormSelect';
 
 export default function Homepage(){
+    const [loaded, setLoaded] = React.useState(false);
+    const [availableData, setAvailableData] = React.useState({});
+    const [searchData, setSearchData] = React.useState({
+        gw: "search_form_json",
+        mkey: "1-40248-2565679",
+        language: 2
+    });
+
+    const updateForm = (newValue, searchName) => {
+        if(searchName === "model"){
+            setSearchData(prevState => ({
+                ...prevState,
+                [searchName]: newValue
+            }))
+        }
+    };
+
+    const refreshForm = () => {
+        setLoaded(false);
+
+        axios.get(
+            'https://content.modix.net/soap/kfz/',
+            {
+                params: searchData
+            }
+        )
+          .then(res => {
+            setAvailableData(res.data);
+            setLoaded(true);
+        })
+    }
+
+    /* componentDidMount & if searchData changes */
+    useEffect(() => {
+        refreshForm();
+    }, [searchData]);
+
     return(
         <>
-            <Card>
-                {/*<CardMedia component="video" src="../assets/videos/homepage-city.mp4" autoPlay/>*/}
-                <CardMedia 
-                    component="img" 
-                    src="https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1583&q=80"
-                />
-            </Card>
-            <Container>
-                <Grid container spacing={4}>
-                    <Grid item xs={12}>
-                        <Typography variant="h3" align="center">Lorem Ipsum.</Typography>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <CardMedia 
-                            component="img" 
-                            src="https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            <Container maxWidth={false} disableGutters={true} sx={{position: 'relative'}}>
+                <Card sx={[{ mb: 15 }]}>
+                    {/*<CardMedia component="video" src="../assets/videos/homepage-city.mp4" autoPlay/>*/}
+                    <CardMedia 
+                        component="img" 
+                        src="https://via.placeholder.com/1920x1080"
+                        sx={{height: '100vh'}}
+                    />
+                    {/*<video loop autoPlay>
+                        <source
+                        src="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_480_1_5MG.mp4"
+                        type="video/mp4"
                         />
-                        <Typography variant="h6" align="center">Your Perfect Samples.</Typography>
-                        <Typography variant="paragraph">With our free and speedy sample service, it's super easy to find the tile you'll love.</Typography>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <CardMedia 
-                            component="img" 
-                            src="https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                        Your browser does not support the video tag.
+                    </video>*/}
+                </Card>
+                <Grid container spacing={3} sx={[
+                    {width: 500},
+                    {background: 'white'}, 
+                    {position: 'absolute'}, 
+                    {bottom: 50}, 
+                    {left: '50%'},
+                    {transform: 'translateX(-50%)'}
+                ]}>
+                    <Grid item xs={6} sx={{p: 2}}>
+                        <FormSelect 
+                            data={availableData.manufacturer} 
+                            label="Manufacturer" 
+                            id="manufacturer"
+                            parentCallback={updateForm}
                         />
-                        <Typography variant="h6" align="center">Friendly Advice.</Typography>
-                        <Typography variant="paragraph">Need some help? Our awesome customer service team can answer any of your tile questions.</Typography>
                     </Grid>
-                    <Grid item xs={4}>
-                        <CardMedia 
-                            component="img" 
-                            src="https://images.unsplash.com/photo-1518458028785-8fbcd101ebb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    <Grid item xs={6} sx={{p: 2}}>
+                        <FormSelect 
+                            data={availableData.model} 
+                            label="Model" 
+                            id="model"
+                            parentCallback={updateForm}
                         />
-                        <Typography variant="h6" align="center">Better For Less.</Typography>
-                        <Typography variant="paragraph">We handpick our tiles to make sure you'll get better tiles, at better prices, than you'll find elsewhere.</Typography>
                     </Grid>
+                </Grid>
+            </Container>
 
+            {/* FAVORITE CARS */}
+            <Container maxWidth="xl" sx={{ my: 15 }}>
+                <Typography variant='h3' sx={[{fontWeight: 'bold'}, {textTransform: 'uppercase'}, {mb: 5}]}>Stock selection.</Typography>
+                <Grid container spacing={4}>
+                    <Grid item xs={4}>
+                        <CardMedia 
+                            component="img" 
+                            src="https://via.placeholder.com/550x330"
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <CardMedia 
+                            component="img" 
+                            src="https://via.placeholder.com/550x330"
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <CardMedia 
+                            component="img" 
+                            src="https://via.placeholder.com/550x330"
+                        />
+                    </Grid>
+                </Grid>
+            </Container>
+
+            {/* COMPANY PHILOSOPHY */}
+            <Container maxWidth="xl" sx={{ my: 15 }}>
+                <Grid container spacing={4}>
                     <Grid item xs={6}>
                         <CardMedia 
                             component="img"
-                            src="https://images.unsplash.com/photo-1525182008055-f88b95ff7980?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            src="https://via.placeholder.com/850"
                         />
                     </Grid>
                     <Grid item xs={6}>
-                        <Typography>Contact us.</Typography>
+                        <Typography variant='h3' sx={[{fontWeight: 'bold'}, {textTransform: 'uppercase'}, {mb: 5}]}>Philosophy.</Typography>
                         <Typography variant="paragraph">Sed nec tincidunt leo. Phasellus risus est, finibus non viverra id, malesuada pulvinar justo. Nunc dolor ipsum, elementum et nisl id, rhoncus condimentum metus. Maecenas scelerisque erat sed dui tempor, facilisis pellentesque tellus maximus. Etiam rhoncus augue quis enim fermentum, vel tristique risus lacinia. Proin cursus urna a dolor malesuada feugiat. Donec mollis nisi quam, a maximus tortor fermentum at. Duis egestas interdum nibh, id fermentum urna vestibulum vitae. Maecenas tincidunt ullamcorper finibus. Mauris egestas lectus nec sem bibendum mollis.</Typography>
                     </Grid>
                 </Grid>
             </Container>
+
+            {/* VEHICLE PROMOTION */}
+            <Container maxWidth={false} disableGutters={true} sx={[{my: 10}, {height: 550}, {position: "relative"}]}>
+                <CardMedia 
+                    component="img" 
+                    src="https://via.placeholder.com/1920x550"
+                    sx={[{position: "absolute"}]}
+                />
+                <Box sx={[{position: "absolute"}, {background: "white"}, {p: 3}, {right: 50}, {bottom: 50}, {width: 450}]}>
+                    <Typography variant='h3' sx={[{fontWeight: 'bold'}, {textTransform: 'uppercase'}, {mb: 5}]}>Promotion.</Typography>
+                    <Typography variant="paragraph">Sed nec tincidunt leo. Phasellus risus est, finibus non viverra id, malesuada pulvinar justo. Nunc dolor ipsum, elementum et nisl id, rhoncus condimentum metus.</Typography>
+                </Box>
+            </Container>
+
+            {/* LOCATIONS */}
+            <Container maxWidth="xl" sx={{ mt: 15, mb: 25 }}>
+                <Typography variant='h3' sx={[{fontWeight: 'bold'}, {textTransform: 'uppercase'}, {mb: 5}]}>Our locations.</Typography>
+                <Grid container spacing={4}>
+                    <Grid item xs={6}>
+                        <CardMedia 
+                            component="img"
+                            src="https://via.placeholder.com/850"
+                        />
+                        <Typography variant='h5' sx={[{fontWeight: 'bold'}, {my: 3}]}>Contact us.</Typography>
+                        <Typography variant="paragraph">Sed nec tincidunt leo. Phasellus risus est, finibus non viverra id, malesuada pulvinar justo. Nunc dolor ipsum, elementum et nisl id, rhoncus condimentum metus. Maecenas scelerisque erat sed dui tempor, facilisis pellentesque tellus maximus. Etiam rhoncus augue quis enim fermentum, vel tristique risus lacinia. Proin cursus urna a dolor malesuada feugiat. Donec mollis nisi quam, a maximus tortor fermentum at. Duis egestas interdum nibh, id fermentum urna vestibulum vitae. Maecenas tincidunt ullamcorper finibus. Mauris egestas lectus nec sem bibendum mollis.</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <CardMedia 
+                            component="img"
+                            src="https://via.placeholder.com/850"
+                        />
+                        <Typography variant='h5' sx={[{fontWeight: 'bold'}, {my: 3}]}>Contact us.</Typography>
+                        <Typography variant="paragraph">Sed nec tincidunt leo. Phasellus risus est, finibus non viverra id, malesuada pulvinar justo. Nunc dolor ipsum, elementum et nisl id, rhoncus condimentum metus. Maecenas scelerisque erat sed dui tempor, facilisis pellentesque tellus maximus. Etiam rhoncus augue quis enim fermentum, vel tristique risus lacinia. Proin cursus urna a dolor malesuada feugiat. Donec mollis nisi quam, a maximus tortor fermentum at. Duis egestas interdum nibh, id fermentum urna vestibulum vitae. Maecenas tincidunt ullamcorper finibus. Mauris egestas lectus nec sem bibendum mollis.</Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+
+            {/* INSTAGRAM */}
         </>
     );
 }
